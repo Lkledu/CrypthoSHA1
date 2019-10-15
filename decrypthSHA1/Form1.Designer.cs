211cf5fd8717d812729a13bf0f29d778117761d0
Eduardo Souza <lkledu@gmail.com>
2019-10-15 09:04:41 -0300

    first commit

---
+++ decrypthSHA1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace decrypthSHA1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelCryptho = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chave para criptografar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Texto criptografado";
+            // 
+            // labelCryptho
+            // 
+            this.labelCryptho.Location = new System.Drawing.Point(15, 114);
+            this.labelCryptho.Name = "labelCryptho";
+            this.labelCryptho.Size = new System.Drawing.Size(400, 20);
+            this.labelCryptho.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 146);
+            this.Controls.Add(this.labelCryptho);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CrypthoSHA1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox labelCryptho;
+    }
+}
+
---
+++ decrypthSHA1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace decrypthSHA1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelCryptho.ReadOnly = true;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            labelCryptho.Text = SenhaAcesso(textBox1.Text);
+        }
+
+
+        private static string SenhaAcesso(string value)
+        {
+
+            byte[] toByte = UTF8Encoding.ASCII.GetBytes(value);
+            byte[] resultado;
+
+            try
+            {
+                using (SHA1Managed sh = new SHA1Managed())
+                {
+                    resultado = sh.ComputeHash(toByte);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao criptografar: " + ex.Message);
+            }
+            return Convert.ToBase64String(resultado);
+        }
+    }
+}
